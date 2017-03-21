1fa0c3e2f5107c2cd17de294e01c295daa862e93
plipp <patricia.lipp@zalando.de>
2017-03-21 16:39:57 +0100

    outcome first session

--- katas/01-Fizz-Buzz/fizz_buzz.py
+++ katas/01-Fizz-Buzz/fizz_buzz.py
@@ -1,8 +1,27 @@
 # actual code
 def fizz_buzz(number):
-    pass
+    result = ""
+    if number % 3 == 0:
+        result = "fizz"
+    if number % 5 == 0:
+        result += "buzz"
+    if result == "":
+        result = number
+    return result
+
+
+def test_buzz():
+    assert fizz_buzz(55) == 'buzz'
+
+
+def test_fizzbuzz():
+    assert fizz_buzz(150) == 'fizzbuzz'
 
 
 # tests
 def test_fizz_buzz():
     assert fizz_buzz(1) == 1
+
+
+def test_fizz_buzz_2():
+    assert fizz_buzz(3) == "fizz"
