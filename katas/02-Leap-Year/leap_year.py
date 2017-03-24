daad67d2d6a8c3c30ddc1ba32770da5072459438
plipp <p.lipp@web.de>
2017-03-24 08:17:23 +0100

    - doctest vs pytest

--- katas/02-Leap-Year/leap_year.py
+++ katas/02-Leap-Year/leap_year.py
@@ -1,8 +1,20 @@
 # actual code
 def leap_year(year: int) -> bool:
+    """
+    Calculate if it is a leap year or not.
+    A leap year is defined as one that
+    - is divisible by 4,
+    - but is not otherwise divisible by 100
+    - unless it is also divisble by 400.
+
+    :param year: the year
+    :return: True if year is a leap year, False else
+
+    >>> leap_year(1) == False
+    """
     pass
 
 
-# tests
+# py-test
 def test_leap_year():
     assert leap_year(1) == False
