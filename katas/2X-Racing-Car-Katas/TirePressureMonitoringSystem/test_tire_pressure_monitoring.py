702330fdbdc765c37fd14d48be026546c1e15ff8
plipp <patricia.lipp@zalando.de>
2017-08-01 08:00:39 +0200

    mocking session outcome

--- katas/2X-Racing-Car-Katas/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
+++ katas/2X-Racing-Car-Katas/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
@@ -1,9 +1,24 @@
+from tire_pressure_monitoring import Alarm, Sensor
+from unittest.mock import patch
 import unittest
 
-from tire_pressure_monitoring import Alarm
 
 class AlarmTest(unittest.TestCase):
-
     def test_alarm_is_off_by_default(self):
         alarm = Alarm()
         assert not alarm.is_alarm_on
+
+    @patch.object(Sensor, 'pop_next_pressure_psi_value')
+    def test_if_alarm_is_on(self, patched_pop_next_pressure_psi_value):
+        patched_pop_next_pressure_psi_value.return_value = 100
+        alarm = Alarm()
+        alarm.check()
+        assert alarm.is_alarm_on
+
+    def test_if_alarm_is_off(self):
+        alarm = Alarm()
+        with patch.object(Sensor, 'pop_next_pressure_psi_value', return_value=20) as fetch_pressure:
+            alarm.check()
+            assert not alarm.is_alarm_on
+        fetch_pressure.assert_called_once()
+
--- katas/2X-Racing-Car-Katas/TirePressureMonitoringSystem/tire_pressure_monitoring.py
+++ katas/2X-Racing-Car-Katas/TirePressureMonitoringSystem/tire_pressure_monitoring.py
@@ -1,13 +1,13 @@
 from sensor import Sensor
 
-class Alarm(object):
 
+class Alarm(object):
     def __init__(self):
         self._low_pressure_threshold = 17
         self._high_pressure_threshold = 21
         self._sensor = Sensor()
         self._is_alarm_on = False
-        
+
     def check(self):
         psi_pressure_value = self._sensor.pop_next_pressure_psi_value()
         if psi_pressure_value < self._low_pressure_threshold \
